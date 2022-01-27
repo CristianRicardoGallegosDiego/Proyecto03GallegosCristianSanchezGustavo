b1a8baf84bc765bfa0691dc7649f12acd06d03ea
Cristian <cristianga2002@ciencias.unam.mx>
2022-01-27 02:52:04 -0600

    FALTO SOLO UN BOTON

--- src/fciencias/edatos/proyecto03/Main.java
+++ src/fciencias/edatos/proyecto03/Main.java
@@ -3,6 +3,7 @@ package fciencias.edatos.proyecto03;
 import java.util.Timer;
 import java.util.Scanner;
 import java.util.Date;
+import java.text.Normalizer;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,18 +23,37 @@ public class Main {
                     String recibe = sc.nextLine();
                     recibe=recibe.replaceAll("\\s+","");
                     if(recibe.equals("1")){
+                        runGame(jugadorUno, true, null);
                         asegurate=false;
                     }else if(recibe.equals("2")){
+                        System.out.println("INGRESA SOLO 9 LETRAS, LAS QUE TU QUIERAS");
+                        boolean reinicia = true;
+                        while(reinicia){
+                            String personal = sc.nextLine();
+                            personal=personal.replaceAll("\\s+","");
+                            personal=personal.toLowerCase();
+                            personal=cleanString(personal);
+                            if(personal.length()!=9 || !contieneSoloLetras(personal)){
+                                System.out.println("INGRESA SOLO 9 LETRAS, LAS QUE TU QUIERAS");
+                            }else{
+                                String[] texto= new String[9];
+                                for (int i = 0; i < texto.length; i++) {
+                                    texto[i]=Character.toString(personal.charAt(i));
+                                }
+                                runGame(jugadorUno, false, texto);
+                                reinicia=false;
+                            }
+                        }
                         asegurate=false;
                     }else{
                         System.out.println("INGRESA SOLO EL NUMERO :D, LAS OPCIONES SON: \n1. MAQUINA GENERE RANDOM\n2. INGRESAR SOLO 9 LETRAS");
                     }
                 }
-                //runGame(jugadorUno);
                 System.out.println("¿QUE MAS DESEA HACER?\n1. JUGAR 1 MINUTO\n2. VER LAS MEJORES ESTADISTICAS\n3. SALIR");
             }else if(decision.equals("2")){
+                //FALTAAAAAA
+                //--------
                 System.out.println("¿QUE MAS DESEA HACER?\n1. JUGAR 1 MINUTO\n2. VER LAS MEJORES ESTADISTICAS\n3. SALIR");
-                //FALTAAA
             }else if(decision.equals("3")){
                 System.out.println("NOS VEMOS :D, CUIDATE!!!!");
                 terminaEjecucion=true;
@@ -44,11 +64,50 @@ public class Main {
        
     }
 
-    public static void runGame(String jugadorUno){
+    /**
+     * Metodo que nos dice sin contien solo cadenas
+     * @param cadena es el texto al que se le quiere sacar 
+     * @return truw si solo contien letras, false en otro caso
+     */
+    public static boolean contieneSoloLetras(String cadena) {
+        for (int x = 0; x < cadena.length(); x++) {
+            char c = cadena.charAt(x);
+            // Si no está entre a y z, ni entre A y Z, ni es un espacio
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * METODO QUE QUITA LOS ACENTOS 
+     * @param texto texto al que se le quitan los acentos 
+     * @return
+     */
+    public static String cleanString(String texto) {
+        texto = Normalizer.normalize(texto, Normalizer.Form.NFD);
+        texto = texto.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        return texto;
+    }
+
+    /**
+     * METODO QUE NOS DICE QUE HACER 
+     * @param jugadorUno nombre del jugador
+     * @param conCualJugar, true si la maquina genera las letras, false si el jugador lo hace
+     * @param arreglo es el arreglo si se decide que va a jugar el jugador
+     */
+    public static void runGame(String jugadorUno, boolean conCualJugar, String[] arreglo){
         System.out.println("Empezemos a jugar jugador "+ jugadorUno+ ": \n \n ");
         Scanner sc = new Scanner(System.in);
         Game juego = new Game(jugadorUno);
-        String[] playerOne = juego.getSecuenciaPlayerOne();
+        String[] playerOne;
+        if(conCualJugar){
+            playerOne = juego.getSecuenciaPlayerOne();
+        }else{
+            playerOne = arreglo;
+        }
+
         String corridaUno = printSecuence(playerOne);
         System.out.print("DEBERAS FORMAR TODAS LAS POSIBLES PALABRAS CON LA SIGUIENTES LETRAS: "+ corridaUno+ 
                         "SOLO TIENES UN MINUTO, TU PUEDES!!!\n");
